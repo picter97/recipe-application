a608e957fd82eb13898ae9d0956184f820edc45a
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-07-09 13:44:01 -0400

    created put router

--- backend/src/routes/recipes.js
+++ backend/src/routes/recipes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import {RecipeModel} from "../models/Recipes.js";
+import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
+import { verifyToken } from "./users.js";
 
 const router = express.Router();
 
@@ -16,7 +18,7 @@ router.get ("/", async (req,res) => {
 });
 
 
-router.post ("/", async (req,res) => {
+router.post ("/", verifyToken,  async (req,res) => {
     const recipe = new RecipeModel(req.body);
     try {
         const response = await recipe.save();
@@ -26,6 +28,42 @@ router.post ("/", async (req,res) => {
 
     }
 });
+
+router.put("/", verifyToken, async (req, res) => {
+    try{
+        const recipe = await RecipeModel.findById(req.body.recipeID);
+        const user = await UserModel.findById(req.body.userID);
+        user.savedRecipes.push(recipe);
+        await user.save();
+        res.json ({savedRecipes: user.savedRecipes});
+       }catch (err) {
+        res.json(err);
+       }
+
+})
+
+router.get("/savedRecipes/ids", async (req, res) => {
+     try{
+        const user = await UserModel.findById.findById(req.body.userID);
+        res.json({ savedRecipes: user?.savedRecipes});
+   } catch (err) {
+        res.json(err)
+     }
+    });
+
+    router.get("/savedRecipes", async (req, res ) => {
+        try {
+        const user = await UserModel.findById(req.bodyuserID);
+        const savedRecipes = await RecipeModel.find({
+            _id: { $in: user.savedRecipes},
+        });
+        res.json({savedRecipes});
+    } catch (err) {
+        res.json(err);
+    }
+    }); 
+
+
 
 export { router as recipesRouter};
 
