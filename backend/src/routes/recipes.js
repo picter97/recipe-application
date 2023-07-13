3abead8d256442c17c9f3383abd32104982b965a
Your Name <teonatskhadaia@yexample.com$ git config --global user.name teona>
2023-07-13 13:12:01 -0400

    creted routes in the recipe.js

--- backend/src/routes/recipes.js
+++ backend/src/routes/recipes.js
@@ -6,69 +6,99 @@ import { verifyToken } from "./users.js";
 
 const router = express.Router();
 
-router.get ("/", async (req,res) => {
-    try{
-        const response = await RecipeModel.find({});
-        res.json(response);
-
-    } catch (err) {
-        res.json(err);
-
-    }
+router.get("/", async (req, res) => {
+  try {
+    const result = await RecipeModel.find({});
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// Create a new recipe
+router.post("/", verifyToken, async (req, res) => {
+  const recipe = new RecipeModel({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    image: req.body.image,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    imageUrl: req.body.imageUrl,
+    cookingTime: req.body.cookingTime,
+    userOwner: req.body.userOwner,
+  });
+  console.log(recipe);
 
-router.post ("/", verifyToken,  async (req,res) => {
-    const recipe = new RecipeModel(req.body);
-    try {
-        const response = await recipe.save();
-        res.json(response);
-    } catch (err) {
-        res.json(err);
-
-    }
+  try {
+    const result = await recipe.save();
+    res.status(201).json({
+      createdRecipe: {
+        name: result.name,
+        image: result.image,
+        ingredients: result.ingredients,
+        instructions: result.instructions,
+        _id: result._id,
+      },
+    });
+  } catch (err) {
+    // console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put("/", verifyToken, async (req, res) => {
-    try{
-        const recipe = await RecipeModel.findById(req.body.recipeID);
-        const user = await UserModel.findById(req.body.userID);
-        user.savedRecipes.push(recipe);
-        await user.save();
-        res.json ({savedRecipes: user.savedRecipes});
-       }catch (err) {
-        res.json(err);
-       }
+// Get a recipe by ID
+router.get("/:recipeId", async (req, res) => {
+  try {
+    const result = await RecipeModel.findById(req.params.recipeId);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-})
+// Save a Recipe
+router.put("/", async (req, res) => {
+  const recipe = await RecipeModel.findById(req.body.recipeID);
+  const user = await UserModel.findById(req.body.userID);
+  try {
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.status(201).json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.get("/savedRecipes/ids", async (req, res) => {
-     try{
-        const user = await UserModel.findById.findById(req.body.userID);
-        res.json({ savedRecipes: user?.savedRecipes});
-   } catch (err) {
-        res.json(err)
-     }
+// Get id of saved recipes
+router.get("/savedRecipes/ids/:userId", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userId);
+    res.status(201).json({ savedRecipes: user?.savedRecipes });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Get saved recipes
+router.get("/savedRecipes/:userId", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userId);
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user.savedRecipes },
     });
 
-    router.get("/savedRecipes", async (req, res ) => {
-        try {
-        const user = await UserModel.findById(req.bodyuserID);
-        const savedRecipes = await RecipeModel.find({
-            _id: { $in: user.savedRecipes},
-        });
-        res.json({savedRecipes});
-    } catch (err) {
-        res.json(err);
-    }
-    }); 
+    console.log(savedRecipes);
+    res.status(201).json({ savedRecipes });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-    router.delete("/:id", async (req, res) => {
-        const result = await RecipeModel.findByIdAndDelete(req.params.id);
-        res.json(result);
-      });
+router.delete("/:id", async (req, res) => {
+    const result = await RecipeModel.findByIdAndDelete(req.params.id);
+    res.json(result);
+    });
 
-
-
-export { router as recipesRouter};
-
+export { router as recipesRouter };
